67984a9eb332c160b9910df9c87fa43b258aac74
I Made .G Agastya Sanskara <41308059+justmadeid@users.noreply.github.com>
2023-12-08 09:08:46 +0700

    Add Library twitter xyber

---
+++ twitter_xyber/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='twitter_xyber',
+    version='0.1.11',
+    packages=find_packages(),
+    install_requires=[
+        'playwright'
+    ],
+)
